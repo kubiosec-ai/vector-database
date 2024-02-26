839bcf2ebde5dfca2d74707ec42beea4417d405b
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-02-26 16:21:40 +0100

    Update main.py

--- llama_index/main.py
+++ llama_index/main.py
@@ -2,13 +2,17 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageCon
 import openai
 import logging
 import sys
+import os
 
+# For detailed logging, uncomment the following 2 lines
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-documents = SimpleDirectoryReader("data").load_data()
+# Set your OS env variable like export OPENAI_API_KEY=xxxxxxxxxxxxxxxxxxx
+openai.api_key=os.getenv("OPENAI_API_KEY")
 
-openai.api_key = "xxxxxxxxxxxxxxx"
+# Load the data files in the ./data directory
+documents = SimpleDirectoryReader("data").load_data()
 
 index = VectorStoreIndex.from_documents(documents)
 index.storage_context.persist()
