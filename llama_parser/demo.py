229e9bda3b9b7e8ac9b77e32d14541517a9a4815
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-03-17 10:27:27 +0100

    Create demo.py

---
+++ llama_parser/demo.py
@@ -0,0 +1,26 @@
+import nest_asyncio
+import os
+
+nest_asyncio.apply()
+
+# https://cloud.llamaindex.ai/api-key
+# wget https://arxiv.org/pdf/1706.03762.pdf -O attention.pdf
+
+
+from llama_parse import LlamaParse
+
+parser = LlamaParse(
+    api_key="llx-xxxx",
+    result_type="markdown",  # "markdown" and "text" are available
+    num_workers=4, 
+    verbose=False,
+    language="en" 
+
+)
+
+file_extractor = {".pdf": parser}
+
+documents = parser.load_data("./attention.pdf")
+
+print(documents[0].text)
+
